d14e4999597b22f204648d47e9cf70121f4b6aa0
ser1372 <ser1372@ukr.net>
2024-12-19 16:32:27 +0200

    added twig components || add galleries

---
+++ src/Form/GalleryType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Gallery;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Validator\Constraints\Image;
+
+class GalleryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('imageFile', VichFileType::class, [
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '2M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PNG or JPEG image.',
+                    ]),
+                ],
+                'allow_delete' => true,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Gallery::class,
+        ]);
+    }
+}
---
+++ src/Twig/Components/Footer/Footer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Twig\Components\Footer;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class Footer
+{
+}
